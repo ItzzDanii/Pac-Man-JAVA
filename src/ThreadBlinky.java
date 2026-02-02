01308f3d70e9529b5c1e336a56e07e28e9150713
DESKTOP-M5T5NK7\barbu <barbuto.daniel29@hotmail.com>
2026-02-02 20:48:37 +0100

    Aggiunta classe ThreadBlinky per gestione del fantasma Blinky

---
+++ src/ThreadBlinky.java
@@ -0,0 +1,34 @@
+public class ThreadBlinky extends Thread {
+    private final MyPanel pannello;
+    private final int delay;
+    private boolean running = true;
+    private Logica logica;
+
+    public ThreadBlinky(MyPanel pannello, int delay) {
+        this.pannello = pannello;
+        this.delay = delay;
+        this.logica = pannello.game_logic;
+    }
+
+    @Override
+    public void run() {
+        while (running) {
+            if (!logica.isReady && !logica.isGameOver()) {
+                logica.moveBlinky(false);
+                logica.checkBlinkyCollision();
+                pannello.repaint();
+            }
+
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException e) {
+                running = false;
+            }
+        }
+    }
+
+    public void stopThread() {
+        running = false;
+        this.interrupt();
+    }
+}

56236abed71643e4c8c17d329b98162276d8d468
DESKTOP-M5T5NK7\barbu <barbuto.daniel29@hotmail.com>
2026-02-03 19:56:49 +0100

    Aggiunta icona della finestra di gioco,cambiato colore della finestra e resa focusable

--- src/SwingPaintDemo1.java
+++ src/SwingPaintDemo1.java
@@ -1,8 +1,15 @@
 import javax.swing.SwingUtilities;
+
+import java.awt.Color;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class SwingPaintDemo1 {
     
+     // icona finestra di gioco
+    static Image game_icon = new ImageIcon("img/game_icon.png").getImage();
     public static void main(String[] args) {
 
         SwingUtilities.invokeLater(new Runnable() {
@@ -17,9 +24,13 @@ public class SwingPaintDemo1 {
         JFrame f = new JFrame("Pac Man");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setSize(250,500);
+
         MyPanel p = new MyPanel();
+        p.setBackground(Color.BLACK);
+        p.requestFocusInWindow();
         f.add(p);
         f.pack();
         f.setVisible(true);
+        f.setIconImage(game_icon);
     }
 }

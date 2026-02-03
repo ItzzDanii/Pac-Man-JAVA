23938cfa8f18d19e6711ab04a4682a90cff66380
DESKTOP-M5T5NK7\barbu <barbuto.daniel29@hotmail.com>
2026-02-03 19:46:23 +0100

    Aggiunta classe SwingPaintDemo1 per gestione finestra di gioco (creazione)

---
+++ src/SwingPaintDemo1.java
@@ -0,0 +1,25 @@
+import javax.swing.SwingUtilities;
+import javax.swing.JFrame;
+
+public class SwingPaintDemo1 {
+    
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+    
+    private static void createAndShowGUI() {
+        
+        JFrame f = new JFrame("Pac Man");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setSize(250,500);
+        MyPanel p = new MyPanel();
+        f.add(p);
+        f.pack();
+        f.setVisible(true);
+    }
+}

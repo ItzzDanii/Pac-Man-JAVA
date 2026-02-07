83c80c21138986ce282914469d32e48fe70efe68
DESKTOP-M5T5NK7\barbu <barbuto.daniel29@hotmail.com>
2026-02-07 15:55:56 +0100

    Aggiunta classe Grafica per disegnare il gioco

---
+++ src/Grafica.java
@@ -0,0 +1,16 @@
+import java.awt.Graphics;
+
+public class Grafica {
+    MyPanel pan;
+    int screenW,screenH;
+
+    public Grafica(MyPanel p,int sW,int sH){
+        this.pan = p;
+        this.screenW = sW;
+        this.screenH = sH;
+    }
+
+    public void drawMap(Graphics g){
+
+    }
+}

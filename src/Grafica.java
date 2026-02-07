7dd72c78c18e43fe62866982eb92b03eb5e0284e
DESKTOP-M5T5NK7\barbu <barbuto.daniel29@hotmail.com>
2026-02-07 16:02:44 +0100

    Aggiunta variabili e metodi per disegnare la mappa di gioco nella classe Grafica

--- src/Grafica.java
+++ src/Grafica.java
@@ -1,16 +1,52 @@
+import java.awt.Color;
 import java.awt.Graphics;
 
 public class Grafica {
     MyPanel pan;
     int screenW,screenH;
+    Logica logic;
 
-    public Grafica(MyPanel p,int sW,int sH){
+    Color cell_fill_color = new Color(53, 48, 148,255),cell_border_color = new Color(18, 16, 67,255); //colori della cella
+    Color ball_fill_color = new Color(240, 114, 37,255),ball_border_color = new Color(44, 22, 12,255); //colori della pallina
+
+
+    public Grafica(MyPanel p,int sW,int sH,Logica l){
         this.pan = p;
         this.screenW = sW;
         this.screenH = sH;
+        this.logic = l;
     }
 
     public void drawMap(Graphics g){
+        if(!logic.isGameOver() || !logic.isLvlCompleted()){
+            int cellW = screenW / SingleTon.getInstance().COLS;
+            int cellH = screenH / SingleTon.getInstance().ROWS;
+            String cell_value = "";
 
+           for (int i = 0; i < SingleTon.getInstance().ROWS; i++) {
+                for (int j = 0; j < SingleTon.getInstance().COLS; j++) {
+                    int x = j * cellW;
+                    int y = i * cellH;
+
+                    cell_value = SingleTon.getInstance().game_map[i][j];
+
+                    switch (cell_value) {
+                        case "EMPTY":
+                            g.setColor(Color.BLACK);
+                            g.fillRect(x, y, cellW, cellH);
+                            
+                        case "WALL":
+                            g.setColor(cell_border_color);
+                            g.drawRect(x, y, cellW, cellH);
+                            g.setColor(cell_fill_color);
+                            g.fillRect(x, y, cellW, cellH);
+                            break;
+                
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
     }
 }
